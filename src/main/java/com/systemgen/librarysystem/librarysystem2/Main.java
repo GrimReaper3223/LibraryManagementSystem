ae6b188e691c148d61c17c75114297d0ffe5b1bc
Deiv <32deiv@gmail.com>
2024-04-07 16:55:37 -0300

    Do-while loop added to 'main' method. Revised exception block. 'Scanner' instance changed

--- src/main/java/com/systemgen/librarysystem/librarysystem2/Main.java
+++ src/main/java/com/systemgen/librarysystem/librarysystem2/Main.java
@@ -1,6 +1,5 @@
 package com.systemgen.librarysystem.librarysystem2;
 
-import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
@@ -11,52 +10,55 @@ import java.util.Scanner;
  * @author deiv
  */
 public class Main {
-
-    static Scanner scanner;
     
     public static void main(String... args) {
-
+        Scanner scanner;
+        int option;
+        
         try {
-            scanner = new Scanner(System.in);
+            do {
+                scanner = new Scanner(System.in);
+                System.out.print("""
+                                   Library System Manager - v2.2.0
+
+                                   Select an option:
+
+                                   1 - Login
+                                   2 - Register
+                                   3 - Exit
+
+                                   >> """);
+
+                option = Integer.parseInt(scanner.nextLine());
+
+                switch(option) {
+                    case 1:
+                        Login loginInstance = new Login();
+                        loginInstance.secureLogin();
+                        main();
+                        break;
+
+                    case 2: 
+                        Register registerInstance = new Register(false);    //default - false
+                        registerInstance.newRegister();
+                        main();
+                        break;
+
+                    case 3: 
+                        System.out.println("\nExiting...\n");
+                        scanner.close();
+                        break;
+
+                    default: 
+                        System.err.println("\nInvalid option\n");
+                }
+                
+            } while(option < 1 || option > 3);
             
-            System.out.print("""
-                               Library System Manager - v2.1
-                               
-                               Select an option:
-                               
-                               1 - Login
-                               2 - Register
-                               3 - Exit
-                               
-                               >> """);
-            
-            int option = scanner.nextInt();
-            
-            switch(option) {
-                case 1:
-                    Login loginInstance = new Login();
-                    loginInstance.secureLogin();
-                    main();
-                    break;
-                    
-                case 2: 
-                    Register registerInstance = new Register(false);    //default - false
-                    registerInstance.newRegister();
-                    main();
-                    break;
-                    
-                case 3: 
-                    System.out.println("\nExiting...\n");
-                    scanner.close();
-                    break;
-                    
-                default: 
-                    System.err.println("\nInvalid option\n");
-                    main();
-            }
-            
-        } catch(InputMismatchException | IOException e) {
-            System.err.println(e.getMessage());
+            //inutiliza a instancia do scanner atual com o valor null, independente da excessao lancada
+        } catch(InputMismatchException ime) {
+            ScannerRegister regScanner = null;
+            ScannerLogin loginScanner = null;
             main();
             
         } catch(NoSuchElementException | IllegalArgumentException exception) {
