9e27611b3304b084bdb9e52c8a47cea45926a7a2
Deiv <32deiv@gmail.com>
2024-04-07 16:51:23 -0300

    'authenticateHelper' method added in 'Authenticate' interface.

--- src/main/java/com/systemgen/librarysystem/librarysystem2/Authenticate.java
+++ src/main/java/com/systemgen/librarysystem/librarysystem2/Authenticate.java
@@ -6,7 +6,8 @@ import java.util.NoSuchElementException;
 
 
 /**
- *
+ * Interface de autenticacao de usuario
+ * 
  * @author deiv
  */
 public interface Authenticate {
@@ -14,7 +15,7 @@ public interface Authenticate {
     // metodo de autenticacao da interface
     // retorna uma instancia box de um tipo desconhecido que implementa a interface authenticate
     default Box<? extends Authenticate> authenticate(String email, char[] password) {
-        String stringPass = new String(password);
+        String stringPass = String.valueOf(password);
         
         //obtem o iterador das listas de usuario/admin
         Iterator<UserRegisterComponent> userIterator = DataStorage.user.listIterator();
@@ -47,4 +48,15 @@ public interface Authenticate {
         //se nenhum usuario for encontrado, lancar uma excessao
         throw new NoSuchElementException("\nError: User not found\n");
     };
+    
+    default Box<?> authenticateHelper(String email, String password) {
+        //cria uma array de chars do tamanho da String password
+        char[] charPassword = new char[password.length()];
+        
+        //obtem os caracteres da String password e os envia para a array charPassword
+        password.getChars(0, password.length(), charPassword, 0);
+        
+        //retorna a instancia de um objeto box de tipo desconhecido que estende de Authenticate
+        return authenticate(email, charPassword);
+    }
 }
